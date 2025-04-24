3e9bfe2269717a4850344c3040e2e71677a7331c
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-04-24 10:48:51 -0600

    chicken nugget pt 2

--- Project/main.py
+++ Project/main.py
@@ -1,5 +1,19 @@
-import InquirerPy
-import os
+from InquirerPy import inquirer
 
 def main():
-    
+    while True:
+        action = inquirer.select(
+            message="Select an action:",
+            choices=[
+                "Play",
+                "User",
+                "Exit",
+            ],
+            default=None,
+        ).execute()
+        if action == "Play":
+            pass
+        elif action == "User":
+            pass
+        elif action == "Exit":
+            break
