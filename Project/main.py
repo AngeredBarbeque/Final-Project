f6c4491351de2ec0dafb7b24f6796c25fc18ef27
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2025-04-30 11:46:39 -0600

    that should work, fixed my part

--- Project/main.py
+++ Project/main.py
@@ -1,20 +1,24 @@
 from InquirerPy import inquirer
+from info import personal_pull
+from settings import *
 
+users = personal_pull()
+user = None
 def main():
     while True:
         action = inquirer.select(
             message="Select an action:",
             choices=[
                 "Play",
-                "User",
+                "User Management",
                 "Exit",
             ],
             default=None,
         ).execute()
         if action == "Play":
             pass
-        elif action == "User":
-            pass
+        elif action == "User Management":
+            user = user_management(user, users)
         elif action == "Exit":
             break
 
--- Project/settings.py
+++ Project/settings.py
@@ -1,47 +1,50 @@
 from InquirerPy import inquirer
 from InquirerPy.validator import EmptyInputValidator
+from login import *
+
 
 def settings():
-        integer_val = inquirer.number(
+    integer_val = inquirer.number(
         message="Use Up and Down arrows to select screen size:",
         min_allowed=10,
         max_allowed=30,
         validate=EmptyInputValidator(),
     ).execute()
+    return integer_val
         
 
 
-def users(user):
-    if user:
-        action = inquirer.select(
-            message="Select an action:",
-            choices=[
-                "Log in",
-                "Exit",
-            ],
-            default=None,
-        ).execute()
-        if action == "Log in":
-            pass
-        elif action == "Change settings":
-            settings()
-        elif action == "Exit":
-            return
-    else:
-        action = inquirer.select(
-            message="Select an action:",
-            choices=[
-                "Log out",
-                "Change settings",
-                "Exit",
-            ],
-            default=None,
-        ).execute()
-        if action == "Log out":
-            pass
-        elif action == "Change settings":
-            settings()
-        elif action == "Exit":
-            return
+def user_management(user, users):
+    while True:
+        if not user:
+            action = inquirer.select(
+                message="Select an action:",
+                choices=[
+                    "Log in",
+                    "Exit",
+                ],
+                default=None,
+            ).execute()
+            if action == "Log in":
+                user = accounts_main(users)
+                continue
+            elif action == "Exit":
+                return user
+        else:
+            action = inquirer.select(
+                message="Select an action:",
+                choices=[
+                    "Log out",
+                    "Change settings",
+                    "Exit",
+                ],
+                default=None,
+            ).execute()
+            if action == "Log out":
+                pass
+            elif action == "Change settings":
+                user['preferences'] = settings()
+            elif action == "Exit":
+                return user
 
 settings()
