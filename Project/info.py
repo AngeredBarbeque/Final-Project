5234ec4d78a0b5c81e10c6d549e5a25838238803
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-05-15 20:23:39 -0600

    Levels almost done :D

--- Project/info.py
+++ Project/info.py
@@ -32,7 +32,7 @@ def personal_pull():
         next(reader)
         users = []
         for row in reader:
-            user = {'name': row[0], 'password': row[1], 'unlocked': int(row[2]), 'scores': [[int(row[3]),int(row[4])],[int(row[5]),int(row[6])],[int(row[7]),int(row[8])],[int(row[9]),int(row[10])],[int(row[11]),int(row[12])],[int(row[13]),int(row[14])],[int(row[15]),int(row[16])],[int(row[17]),int(row[18])],[int(row[19]),int(row[20])],[int(row[21]),int(row[22])],[int(row[23]),int(row[24])],[int(row[25]),int(row[26])],[int(row[27]),int(row[28])],[int(row[29]),int(row[30])],[int(row[31]),int(row[32])]],'preferences':int(row[33])}
+            user = {'name': row[0], 'password': row[1], 'unlocked': int(row[2]), 'scores': [[float(row[3]),int(row[4])],[float(row[5]),int(row[6])],[float(row[7]),int(row[8])],[float(row[9]),int(row[10])],[float(row[11]),int(row[12])],[float(row[13]),int(row[14])],[float(row[15]),int(row[16])],[float(row[17]),int(row[18])],[float(row[19]),int(row[20])],[float(row[21]),int(row[22])],[float(row[23]),int(row[24])],[float(row[25]),int(row[26])],[float(row[27]),int(row[28])],[float(row[29]),int(row[30])],[float(row[31]),int(row[32])]],'preferences':int(row[33])}
             users.append(user)
         return users
 
@@ -43,16 +43,16 @@ def overall_pull():
         level_scores = []
         for row in reader:
             score = {
-                '1':[int(row[0]), int(row[1]), row[2]],
-                '2':[int(row[3]), int(row[4]), row[5]],
-                '3':[int(row[6]), int(row[7]), row[8]],
-                '4':[int(row[9]), int(row[10]), row[11]],
-                '5':[int(row[12]), int(row[13]), row[14]],
-                '6':[int(row[15]), int(row[16]), row[17]],
-                '7':[int(row[18]), int(row[19]), row[20]],
-                '8':[int(row[21]), int(row[22]), row[23]],
-                '9':[int(row[24]), int(row[25]), row[26]],
-                '10':[int(row[27]), int(row[28]), row[29]],
+                '1':[float(row[0]), int(row[1]), row[2]],
+                '2':[float(row[3]), int(row[4]), row[5]],
+                '3':[float(row[6]), int(row[7]), row[8]],
+                '4':[float(row[9]), int(row[10]), row[11]],
+                '5':[float(row[12]), int(row[13]), row[14]],
+                '6':[float(row[15]), int(row[16]), row[17]],
+                '7':[float(row[18]), int(row[19]), row[20]],
+                '8':[float(row[21]), int(row[22]), row[23]],
+                '9':[float(row[24]), int(row[25]), row[26]],
+                '10':[float(row[27]), int(row[28]), row[29]],
             }
             level_scores.append(score)
     return level_scores
